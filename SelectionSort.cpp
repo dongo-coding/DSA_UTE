bf94ea0aab3e3a8721a6635f9baee6d28cc21c6a
Do Ngo <bahuynh619@gmail.com>
2025-11-24 10:25:17 +0700

    Update SelectionSort.cpp

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -1,4 +1,6 @@
-void selectionSort(int a[]) {
+#include <bits/stdc++.h>
+using namespace std;
+void selectionSort(int a[],int n) {
     for (int i = 0; i < n - 1; i++) {
         int min_idx = i;
         for (int j = i + 1; j < n; j++) {
@@ -7,5 +9,31 @@ void selectionSort(int a[]) {
             }
         }
         swap(a[i], a[min_idx]);
+    }
+}
+int main ()
+{
+    int n;
+    cout<<"Nhap n: ";
+    cin>>n;
+    cin.ignore();
+    int a[1000000];
+    int cnt=0;
+    
+    string dong;
+    getline (cin,dong);
+    
+    stringstream ss(dong);
+    string temp;
+    while(getline(ss,temp,',')){
+        a[cnt++]=atoi(temp.c_str());
+    }
+    
+    selectionSort(a,n);
+    
+    cout<<"Array after using Selection sort: ";
+    
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
     }
 }
